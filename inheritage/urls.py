244c6d0f1e6ed1de3889a518d0e5858262137a39
Katarina Rossi <katarina.n.rossi@gmail.com>
2017-01-30 09:51:22 -0800

    Set up base html template and static assets folder

--- inheritage/urls.py
+++ inheritage/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from src import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.root, name='root'),
 ]
