25d0fffeb05d053a319c579303ede8bebd12d420
Katarina Rossi <katarina.n.rossi@gmail.com>
2017-01-30 11:36:31 -0800

    Add profile, user_ethnicities, and ethnicities models

--- src/models.py
+++ src/models.py
@@ -2,4 +2,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey(User, null=True, blank=True)
+    name = models.CharField(null=False)
+
+class UserEthnicities(models.Model):
+    profile = models.ForeignKey(Profile, null=False)
+    ethnicity = models.ForeignKey(Ethnicity, null=False)
+    percent = models.IntegerField()
+
+class Ethnicities(models.Model):
+    name = models.charField(null=False)
