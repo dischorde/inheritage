e9be759fec12952ab3bf9da7e8f05ed10729cd2b
Chloe <chloejchou@gmail.com>
2017-01-30 10:04:51 -0800

    add frontend folder with root reducer, store, and middleware

---
+++ frontend/components/root.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+
+const Root = ({store}) => {
+  return (
+    <p>Hello</p>
+  );
+};
+
+export default Root;
