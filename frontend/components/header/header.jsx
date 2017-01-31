1df057b6640187f66272fcc42241d8c6fbf70a2c
Chloe <chloejchou@gmail.com>
2017-01-30 18:40:26 -0800

    ignore pyc files

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -6,6 +6,7 @@ class Header extends React.Component {
       <div>
         <div id="header">
           <h1>inheritage</h1>
+          <i className="fa fa-question-circle" aria-hidden="true"></i>
         </div>
         {this.props.children}
       </div>
