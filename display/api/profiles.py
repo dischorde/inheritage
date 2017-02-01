8c716a7a9dbdbb05b8fa0d55d17495dde8b9d713
Katarina Rossi <katarina.n.rossi@gmail.com>
2017-01-31 22:47:30 -0800

    Write profile resource

--- display/api/profiles.py
+++ display/api/profiles.py
@@ -1,6 +1,7 @@
 from restless.dj import DjangoResource
 
 from display.models import Profile
+from display.models import DataPoint
 
 class ProfileResource(DjangoResource):
     def detail(self, pk):
@@ -8,10 +9,25 @@ class ProfileResource(DjangoResource):
         ethnicity_set = profile.ethnicities.all()
         ethnicities = []
 
-        # for ethnicity in ethnicity_set:
+        for ethnicity in ethnicity_set:
+            snps = []
+            eth_ids = ethnicity.populations.all().values('id')
+            data_points = DataPoint.objects.filter(sub_population_id__in=eth_ids)
+            for dp in data_points:
+                dp_detail = {
+                    id: dp.id,
+                    percent: dp.percent
+                    name: dp.snp.name,
+                    summary: dp.snp.summary
+                }
+                snps.append(dp_detail)
 
-
-
+            detail = {
+                name: "Italian",
+                percent: "50%",
+                snps: snps
+            }
+            ethnicities.append(detail)
 
         return {
             'profileId': profile.id,
