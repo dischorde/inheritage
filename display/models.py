7ec99a0fa985525b365a6d62a2f6b3cd3ddd2573
Katarina Rossi <katarina.n.rossi@gmail.com>
2017-01-31 19:35:30 -0800

    Add __str__ to models and many to many relationship on profile

--- display/models.py
+++ display/models.py
@@ -3,32 +3,42 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
-class Profile(models.Model):
-    user = models.ForeignKey(User, null=True, blank=True)
+
+class Snp(models.Model):
     name = models.CharField(max_length=255, null=False)
+    summary = models.TextField(null=False)
+    description = models.TextField(null=False)
+    def __str__(self):
+        return self.name
 
 class SubPopulation(models.Model):
     name = models.CharField(max_length=255,null=False)
+    code = models.CharField(max_length=5,null=False)
     specific = models.BooleanField(null=False)
+    def __str__(self):
+        return self.name
+
+class DataPoint(models.Model):
+    snp = models.ForeignKey(Snp, null=False)
+    sub_population = models.ForeignKey(SubPopulation, null=False)
+    percent = models.IntegerField(null=False)
 
 class Ethnicity(models.Model):
     name = models.CharField(max_length=255, null=False)
     lat = models.DecimalField(max_digits=9, decimal_places=6)
     lon = models.DecimalField(max_digits=9, decimal_places=6)
     populations = models.ManyToManyField(SubPopulation)
+    def __str__(self):
+        return self.name
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, null=True, blank=True)
+    name = models.CharField(max_length=255, null=False)
+    ethnicities = models.ManyToManyField(Ethnicity, through='UserEthnicity')
+    def __str__(self):
+        return self.name
 
 class UserEthnicity(models.Model):
     profile = models.ForeignKey(Profile, null=False)
     ethnicity = models.ForeignKey(Ethnicity, null=False)
     percent = models.IntegerField()
-
-
-class Snp(models.Model):
-    name = models.CharField(max_length=255, null=False)
-    summary = models.TextField(null=False)
-    description = models.TextField(null=False)
-
-class DataPoint(models.Model):
-    snp = models.ForeignKey(Snp, null=False)
-    sub_population = models.ForeignKey(SubPopulation, null=False)
-    percent = models.IntegerField(null=False)
