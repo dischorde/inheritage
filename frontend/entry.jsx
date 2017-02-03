d67f6543b0809821052519bf3cd4428c972fcd38
lmadigan <laurenmadigan51@gmail.com>
2017-02-03 10:03:26 -0800

    profile actions and reducer

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
+import createProfile from './actions/profile_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
-
+  window.store = store;
+  window.createProfile = createProfile;
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store}/>, root);
 });
