b6840b9a5c54e108e8ccc030e493b927d3780a69
bronwyndunn <bronwynleigh@berkeley.edu>
2017-02-04 13:50:58 -0800

    add img and styling to snp detail

--- frontend/components/snp/snp_detail.jsx
+++ frontend/components/snp/snp_detail.jsx
@@ -24,15 +24,18 @@ class SnpDetail extends React.Component {
     for (var point in snpData) {
       snpArr.push(snpData[point]);
     }
-    
+
     const data = snpArr;
     return(
-      <div id="snp-detail-container">
-        <div className="snp-detail-description">
-          <h1>{this.props.snp.name}</h1>
-          <p>{this.props.snp.description}</p>
+      <div>
+        <div className="snp-detail-container">
+          <div className="snp-detail-img">
+            <h1>{this.props.snp.name}</h1>
+            <p className="snp-description">{this.props.snp.description}</p>
+          </div>
         </div>
-        <div>
+
+        <div className="bar-container">
           <BarChart width={600} height={300} data={data}
             margin={{top: 5, right: 30, left: 20, bottom: 5}}>
             <XAxis dataKey="name"/>
