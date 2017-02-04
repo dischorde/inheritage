b79aaa5b4185775058a7020992cc36af2b3a477e
Chloe <chloejchou@gmail.com>
2017-02-03 18:11:24 -0800

    add snp actions/reducer

--- frontend/components/snp/snp_detail_container.js
+++ frontend/components/snp/snp_detail_container.js
@@ -1,12 +1,13 @@
 import { connect } from 'react-redux';
 import SnpDetail from './snp_detail';
+import { requestSnp } from '../../actions/snp_actions';
 
-const mapStateToProps = state => {
+const mapStateToProps = state => ({
+  snp: state.snp
+});
 
-};
-
-const mapDispatchToProps = dispatch => {
-
-};
+const mapDispatchToProps = dispatch => ({
+  requestSnp: (id) => dispatch(requestSnp(id))
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(SnpDetail);
